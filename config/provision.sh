494d5cec1bb22286483b699e0536e65ae465f94b
Mattia Rombi <mattyro96@gmail.com>
2025-02-09 16:01:04 +0100

    Added Docker installation

--- config/provision.sh
+++ config/provision.sh
@@ -41,6 +41,9 @@ bash --login /vagrant/config/provision/java.sh 2>&1 | tee -i /vagrant/config/pro
 echo "Installing/Configuring Tomcat..."
 bash --login /vagrant/config/provision/tomcat.sh 2>&1 | tee -i /vagrant/config/provision/tomcat.log >/dev/null
 
+echo "Installing/Configuring Docker..."
+bash --login /vagrant/config/provision/docker.sh 2>&1 | tee -i /vagrant/config/provision/docker.log >/dev/null
+
 echo "Installing extra packages..."
 bash --login /vagrant/config/provision/extra_packages.sh 2>&1 | tee -i /vagrant/config/provision/extra_packages.log >/dev/null
 
