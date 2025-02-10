2a05c00cd7371d31fc260e76048157ad6e99006b
Mattia Rombi <mrombi@siatass.com>
2025-02-10 13:50:58 +0100

    Added Brew installation

---
+++ config/provision/brew.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm # git
+yum -y install http://www.city-fan.org/ftp/contrib/yum-repo/rhel7/x86_64/city-fan.org-release-3-16.rhel7.noarch.rpm # curl
+
+yum-config-manager -q --enable endpoint
+yum-config-manager -q --enable city-fan.org
+
+yum -y install curl git
+
+NONINTERACTIVE=1 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' > /etc/profile.d/brew.sh
+chmod +x /etc/profile.d/brew.sh
+source /etc/profile.d/brew.sh
