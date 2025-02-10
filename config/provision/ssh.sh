cbf2fcdc563cc70f98af6c353daa75a67a48313e
Mattia Rombi <mrombi@siatass.com>
2025-02-10 17:52:21 +0100

    Updated SSH installation

--- config/provision/ssh.sh
+++ config/provision/ssh.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+yum -y install openssh-server openssh-clients
+
+if [[ "$(hostnamectl --static)" == "vagrant.local" ]]; then
+    sed -i -E 's/^\s*#?\s*Port\s*[0-9]+\s*$/Port 2222/' /etc/ssh/sshd_config
+fi
+
+sed -i -E 's/^\s*#?\s*PasswordAuthentication\s*\S+\s*$/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+systemctl enable --now sshd
