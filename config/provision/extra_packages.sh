590e6d70b569519aa985a0d48073fb726e57ff02
Mattia Rombi <mrombi@siatass.com>
2025-02-10 14:12:57 +0100

    Removed Psychotic repo

--- config/provision/extra_packages.sh
+++ config/provision/extra_packages.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 yum -y install https://mirrors.coreix.net/li.nux.ro/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm # msttcore-fonts
-yum -y install https://packages.psychotic.ninja/7/base/x86_64/RPMS/psychotic-release-1.0.0-1.el7.psychotic.noarch.rpm
 
 yum-config-manager -q --enable nux-dextop
-yum-config-manager -q --enable psychotic
-yum-config-manager -q --enable psychotic-plus
 
 yum -y install msttcore-fonts-installer urw-base35-nimbus-sans-fonts
 yum -y install bzip2-devel libcurl-devel libxslt-devel libzip-devel oniguruma-devel readline-devel
