7db3406d8255d6c7aedd27147145be107532cf90
Mattia Rombi <mrombi@siatass.com>
2025-02-10 10:17:40 +0100

    Updated git and cURL repo

--- config/provision/basic_packages.sh
+++ config/provision/basic_packages.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm # git
+yum -y install http://www.city-fan.org/ftp/contrib/yum-repo/rhel7/x86_64/city-fan.org-release-3-16.rhel7.noarch.rpm # curl
+
+yum-config-manager --enable endpoint
+yum-config-manager --enable city-fan.org
+
 yum -y groupinstall 'Development Tools'
+
+yum -y install \
+    curl \
+    git
 
 yum -y install \
     nfs-utils \
