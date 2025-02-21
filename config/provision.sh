4b07a31569ff2b3efc01aff02cb61099e9a62390
Mattia Rombi <mattyro96@gmail.com>
2025-02-21 12:00:40 +0100

    Updated scripts to support WSL

--- config/provision.sh
+++ config/provision.sh
@@ -50,6 +50,9 @@ bash --login /vagrant/config/provision/mercure.sh 2>&1 | tee -i /vagrant/config/
 echo "Installing/Configuring Snapd..."
 bash --login /vagrant/config/provision/snapd.sh 2>&1 | tee -i /vagrant/config/provision/snapd.log >/dev/null
 
+echo "Installing/Configuring Util-Linux..."
+bash --login /vagrant/config/provision/util-linux.sh 2>&1 | tee -i /vagrant/config/provision/util-linux.log >/dev/null
+
 echo "Installing extra packages..."
 bash --login /vagrant/config/provision/extra_packages.sh 2>&1 | tee -i /vagrant/config/provision/extra_packages.log >/dev/null
 
