dc20a2b152748f1aeca1824de5dc880a80b9b381
Mattia Rombi <mattyro96@gmail.com>
2025-02-23 13:56:35 +0100

    Updated SELinux installation

--- config/provision/selinux.sh
+++ config/provision/selinux.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+yum -y install \
+    selinux-policy \
+    selinux-policy-targeted \
+    policycoreutils \
+    policycoreutils-python \
+    libselinux-utils
+
 setenforce 0
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+sed -i 's/^SELINUXTYPE=.*/SELINUXTYPE=targeted/' /etc/selinux/config
